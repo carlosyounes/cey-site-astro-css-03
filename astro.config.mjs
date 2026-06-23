d5c39e974eb2104e9420bd48b58852bc5f756518
Carlos Emilio Younes <104083197+carlosyounes@users.noreply.github.com>
2026-06-23 19:37:35 +0000

    fix: adiciona site e base para corrigir o css quebrado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://github.io', // Seu link base do GitHub
+  base: '/cey-site-astro-css-03',         // O nome exato do seu repositório
+});
